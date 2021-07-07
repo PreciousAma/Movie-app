de8154712ba3fa3aa3f5dc11233778fc79980790
AmaraCP <preshchilaka06@gmail.com>
2021-07-07 16:04:32 +0100

    called api for categories movies

--- src/components/Movies/Categories.js
+++ src/components/Movies/Categories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card2 from '../../reuseables/Card2';
 import PageButtons from '../../reuseables/PageButtons';
 import '../../styles/Movies.css';
@@ -7,25 +7,41 @@ import Manikarnika from '../../images/Manikarnika.png';
 import Uri from '../../images/Uri.png';
 import Singham from '../../images/Singham.png';
 import Thackeray from '../../images/Thackeray.png';
+import clsx from 'clsx';
+import { Api } from '../../utils/Api';
 
 const tabs = {
-    latest: 1,
-    popular: 2,
-    upcoming: 3
+    popular: 1,
+    upcoming: 2
 };
 
 
 const Categories = () => {
-    const [activeTab, setActiveTab] = useState('latest');
+    const [activeTab, setActiveTab] = useState('popular');
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getMovies();
+    }, [activeTab]);
+
+    const getMovies = async () => {
+        try {
+            const { data } = await Api.get(`/movie/${activeTab}`);
+            setData(data.results);
+            console.log(data);
+        }catch (error) {
+            const errorMessage = error.isAxiosError ? error.response.data.status_message : error.message;
+            console.error({ errorMessage });
+        }
+    }
 
     return (
         <>
             <div className="categories">
                 <div className="categories__nav--items">
                     <ul className="nav__links">
-                        <li className={`nav__link ${tabs[activeTab] === 1 && 'active'}`} onClick={() => setActiveTab('latest')}>Latest</li>
-                        <li className={`nav__link ${tabs[activeTab] === 2 && 'active'}`} onClick={() => setActiveTab('popular')}>Popular</li>
-                        <li className={`nav__link ${tabs[activeTab] === 3 && 'active'}`} onClick={() => setActiveTab('upcoming')}>Upcoming</li>
+                        <li className={clsx('nav__link', tabs[activeTab] === 1 && 'active')} onClick={() => setActiveTab('popular')}>Popular</li>
+                        <li className={clsx('nav__link', tabs[activeTab] === 2 && 'active')} onClick={() => setActiveTab('upcoming')}>Upcoming</li>
                     </ul>
                     <div><PageButtons /></div>   
                 </div>
