3c21c1806ed07b076e517855214ad6581fecaf16
AmaraCP <preshchilaka06@gmail.com>
2021-07-09 17:47:10 +0100

    implemented top rated api

--- src/components/Movies/Categories.js
+++ src/components/Movies/Categories.js
@@ -1,14 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Card2 from '../../reuseables/Card2';
-import PageButtons from '../../reuseables/PageButtons';
+import React, { useRef, useState } from 'react';
 import '../../styles/Movies.css';
-import Kesari from '../../images/Kesari.png';
-import Manikarnika from '../../images/Manikarnika.png';
-import Uri from '../../images/Uri.png';
-import Singham from '../../images/Singham.png';
-import Thackeray from '../../images/Thackeray.png';
 import clsx from 'clsx';
-import { Api } from '../../utils/Api';
+import Popular from './Popular';
+import Upcoming from './Upcoming';
 
 const tabs = {
     popular: 1,
@@ -18,22 +12,7 @@ const tabs = {
 
 const Categories = () => {
     const [activeTab, setActiveTab] = useState('popular');
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        getMovies();
-    }, [activeTab]);
-
-    const getMovies = async () => {
-        try {
-            const { data } = await Api.get(`/movie/${activeTab}`);
-            setData(data.results);
-            console.log(data);
-        }catch (error) {
-            const errorMessage = error.isAxiosError ? error.response.data.status_message : error.message;
-            console.error({ errorMessage });
-        }
-    }
+    const flicking = useRef(null);
 
     return (
         <>
@@ -43,17 +22,10 @@ const Categories = () => {
                         <li className={clsx('nav__link', tabs[activeTab] === 1 && 'active')} onClick={() => setActiveTab('popular')}>Popular</li>
                         <li className={clsx('nav__link', tabs[activeTab] === 2 && 'active')} onClick={() => setActiveTab('upcoming')}>Upcoming</li>
                     </ul>
-                    <div><PageButtons /></div>   
                 </div>
             </div>
-
-            <div className="card2__category">
-                <Card2 image={Uri} />
-                <Card2 image={Manikarnika} />
-                <Card2 image={Thackeray} />
-                <Card2 image={Kesari} />
-                <Card2 image={Singham} />
-            </div>
+            {activeTab === 'popular' && <Popular ref={flicking} />}
+            {activeTab === 'upcoming' && <Upcoming ref={flicking} />}
         </>
     )
 }
